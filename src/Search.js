b3a669b3f2bcb51f883512059b525898c7b5ecfe
Ankur Bajpai <88947414+ankurbajpai125@users.noreply.github.com>
2024-06-18 01:31:52 +0530

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ const Search = () => {
   const {query,searchPost} = useGlobalContext();
   return (
     <>
-      <h1>Ankur Technical Tech Website</h1>
+      <h1>Tech Website</h1>
       <form onSubmit={(e)=> e.preventDefault()}>
         <div>
           <input type='text' placeholder='search here' value={query} onChange={(e)=> searchPost(e.target.value)} />
